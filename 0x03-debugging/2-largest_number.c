8266f5431dc33a439d9669a09b0b021b4ed5626d
Raniaelmorabet <raniaelmorabet9@gmail.com>
2023-02-23 04:26:18 -0800

    largest_number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,26 +5,30 @@
  * @a: first integer
  * @b: second integer
  * @c: third integer
- * Return: the largest number
+ * Return: largest number
  */
 
 int largest_number(int a, int b, int c)
-
 {
-	int largest;
+int largest;
 
-	if (a > b && a > c)
-	{
-		largest = a;
-	}
-	else if (b > a && b > c)
-	{
-		largest = b;
-	}
-	else if (c > a && c > b)
-	{
-		largest = c;
-	}
+if (a > b && a > c)
+{
+largest = a;
+}
+else if (b > c && b > a)
+{
+largest = b;
+}
+else if (c > b)
+{
+largest = c;
+}
+else
+{
+largest = b;
+}
 
-	return (largest);
+
+return (largest);
 }
