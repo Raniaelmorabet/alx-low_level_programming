3e74b818a0bebd96ea4dcc722f924e8c2406b660
Raniaelmorabet <raniaelmorabet9@gmail.com>
2023-02-23 10:16:10 -0800

    isdigit

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
- * main - check the code.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char c;
+    char c;
 
-	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
-	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
-	return (0);
+    c = '0';
+    printf("%c: %d\n", c, _isdigit(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isdigit(c));
+    return (0);
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,4 +5,6 @@ int _isupper(int c);
 
 int _putchar(char n);
 
+int _isdigit(int c);
+
 #endif
