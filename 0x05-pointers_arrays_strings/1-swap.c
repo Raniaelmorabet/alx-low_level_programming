64f47e93a288a4ba2bba3d894a82fb4299ab4556
Raniaelmorabet <raniaelmorabet9@gmail.com>
2023-02-27 12:18:47 -0800

    a function that returns the length of a string

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -9,6 +9,7 @@
 void swap_int(int *a, int *b)
 {
 	int c;
+
 	c = *a;
 	*a = *b;
 	*b = c;
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * _strlen - a function that returns the length of a string
+  *
+  * @s: a pointer
+  *
+  * Return: always 0 (success)
+  */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	return (i);
+}
