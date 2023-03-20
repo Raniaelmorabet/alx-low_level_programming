0f322566eb0967c7e076d8d62fb063e50904e487
“Raniaelmorabet” <raniaelmorabet9@gmail.com>
2023-03-20 15:57:06 -0700

    a new type struct dog with the following elements

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+  * struct dog - prototype for a dog
+  * @name: the name of the dog
+  * @age: the age of the dog
+  * @owner: the owner of the dog
+  *
+  * Description: this is prototype for the dog
+  */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-#endif /* dog.h */
+#endif /* DOG_H */
