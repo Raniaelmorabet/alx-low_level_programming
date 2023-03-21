35fe38dd9e3e80ff27a8a510aafdd2641c3fb643
“Raniaelmorabet” <raniaelmorabet9@gmail.com>
2023-03-20 17:26:34 -0700

    a function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,37 +1,74 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
-  * new_dog - creates a new dog
-  * @name: the name of the new dog
-  * @age: the age of the new dog
-  * @owner: the owner of the new dog
-  *
-  * Return: new dog
-  */
+ * _strlen - returns length of string
+ * @s: string
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+
+	return (i);
+}
+
+/**
+ * _strcpy - copies string
+ * @dest: destination
+ * @src: source
+ *
+ * Return: pointer to destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: pointer to new dog
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *p;
-	/* reserving memory to struct*/
-	p = malloc(sizeof(dog_t));
-	if (p == NULL)
+	dog_t *new_dog;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (!new_dog)
 		return (NULL);
-	/* Cpunting name pointer*/
-	if (name == NULL)
+
+	new_dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (!new_dog->name)
 	{
-		free(p);
-		free(owner);
+		free(new_dog);
 		return (NULL);
 	}
-	if (owner == NULL)
+	new_dog->name = _strcpy(new_dog->name, name);
+
+	new_dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (!new_dog->owner)
 	{
-		free(p);
-		free(name);
+		free(new_dog->name);
+		free(new_dog);
 		return (NULL);
 	}
-	p->name = name;
-	p->age = age;
-	p->owner = owner;
-	return (p);
+	new_dog->owner = _strcpy(new_dog->owner, owner);
+
+	new_dog->age = age;
+
+	return (new_dog);
 }
