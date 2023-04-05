f2f84df630780fc6881394adcf901070b2b9a850
“Raniaelmorabet” <raniaelmorabet9@gmail.com>
2023-04-04 17:54:30 -0700

    deleting files

--- 0x0D-preprocessor/0-main.c
+++ 0x0D-preprocessor/0-main.c
@@ -1,6 +1,6 @@
-#include "0-object_like_macro.h"
-#include "0-object_like_macro.h"
 #include <stdio.h>
+#include "3-function_like_macro.h"
+#include "3-function_like_macro.h"
 
 /**
  * main - check the code
@@ -9,9 +9,11 @@
  */
 int main(void)
 {
-    int s;
+    int i;
+    int j;
 
-    s = 98 + SIZE;
-    printf("%d\n", s);
+    i = ABS(-98) * 10;
+    j = ABS(98) * 10;
+    printf("%d, %d\n", i, j);
     return (0);
 }
