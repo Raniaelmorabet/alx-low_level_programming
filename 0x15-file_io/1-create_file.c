541cb8ab01be32cfc71c4b9d6303da139e68c7bb
“Raniaelmorabet” <raniaelmorabet9@gmail.com>
2023-04-09 10:45:52 -0700

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,35 +10,24 @@ int create_file(const char *filename, char *text_content)
 {
 	int f, l, write_bytes;
 
-	/* check if filename is NULL */
+
 	if (filename == NULL)
 		return (-1);
-
-	/* open file with O_CREAT, O_WRONLY, O_TRUNC */
 	f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (f == -1)
 		return (-1);
-
-	/* if text_content is NULL, write nothing to the file */
 	if (text_content == NULL)
 	{
 		close(f);
 		return (1);
 	}
-
-	/* calculate length of text_content */
-	for (l = 0; text_content[len]; l++)
-		;
-
-	/* write text_content to the file */
+	for (l = 0; text_content[l]; l++)
 	write_bytes = write(f, text_content, l);
 	if (write_bytes == -1)
 	{
 		close(f);
 		return (-1);
 	}
-
-	/* close file and return 1 on success */
 	close(f);
 	return (1);
 }
