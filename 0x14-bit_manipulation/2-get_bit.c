21ddd320506a6af99dc37e8d44757051ecca227c
“Raniaelmorabet” <raniaelmorabet9@gmail.com>
2023-04-08 19:09:07 -0700

    a function that prints the binary representation of a number.

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * get_bit - returns the value of a bit at a given index
  * @n: the number to get the bit from
@@ -9,7 +8,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    if (index > sizeof(n) * 8)
-        return (-1);
-    return ((n >> index) & 1);
+	if (index > sizeof(n) * 8)
+		return (-1);
+	return ((n >> index) & 1);
 }
