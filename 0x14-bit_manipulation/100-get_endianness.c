9c0d6f43637542e748ac76461a2733f54aee3675
“Raniaelmorabet” <raniaelmorabet9@gmail.com>
2023-04-08 19:12:34 -0700

    a function that checks the endianness.

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,13 +1,12 @@
 #include "main.h"
-
 /**
  * get_endianness - checks the endianness.
  * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
 {
-    unsigned int n = 1;
-    char *d = (char *)&n;
+	unsigned int n = 1;
+	char *d = (char *)&n;
 
-    return ((int)*d);
+	return ((int)*d);
 }
