286b3ed77e1b43d312d17f1477a22c42b211c6a2
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-05-31 22:02:17 +0200

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,6 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void copy_file(const char *file_from, const char *file_to);
-
+void print_error(const char *msg)
+void print_elf_header(const ElfHeader *header)
 
 #endif /* MAIN_H */
