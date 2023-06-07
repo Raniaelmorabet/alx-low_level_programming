c7c24a2d1de76dbf6edeecbaae339eb83bdb099a
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-06-07 19:31:34 +0200

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -5,111 +5,142 @@
 
 #define BUF_SIZE 64
 
-typedef struct {
+typedef struct
+{
     unsigned char magic[4];
     unsigned char class;
     unsigned char data;
     unsigned char version;
     unsigned char osabi;
     unsigned char abiversion;
-    unsigned char type[2];
-    unsigned char entry[8];
+    unsigned char padding[7];
+    unsigned short type;
+    unsigned short machine;
+    unsigned int version2;
+    unsigned long entry;
+    unsigned long phoff;
+    unsigned long shoff;
+    unsigned int flags;
+    unsigned short ehsize;
+    unsigned short phentsize;
+    unsigned short phnum;
+    unsigned short shentsize;
+    unsigned short shnum;
+    unsigned short shstrndx;
 } ElfHeader;
 
-void print_error(const char *msg) {
-    fprintf(stderr, "%s\n", msg);
+void print_error(const char *error_message)
+{
+    fprintf(stderr, "Error: %s\n", error_message);
     exit(98);
 }
 
-void print_elf_header(const ElfHeader *header) {
+void print_elf_header(const ElfHeader *header)
+{
     printf("ELF Header:\n");
-    printf("  Magic:   %02x %02x %02x %02x\n", header->magic[0], header->magic[1], header->magic[2], header->magic[3]);
-    printf("  Class:                             ");
-    switch (header->class) {
-        case 1:
-            printf("ELF32\n");
-            break;
-        case 2:
-            printf("ELF64\n");
-            break;
-        default:
-            printf("<unknown>\n");
-            break;
-    }
-    printf("  Data:                              ");
-    switch (header->data) {
-        case 1:
-            printf("2's complement, little endian\n");
-            break;
-        case 2:
-            printf("2's complement, big endian\n");
-            break;
-        default:
-            printf("<unknown>\n");
-            break;
-    }
+    printf("  Magic:   %02x %02x %02x %02x\n",
+           header->magic[0], header->magic[1], header->magic[2], header->magic[3]);
+    printf("  Class:                             ELF%d\n", header->class == 1 ? 32 : 64);
+    printf("  Data:                              %s\n", header->data == 1 ? "2's complement, little endian" : "2's complement, big endian");
     printf("  Version:                           %d (current)\n", header->version);
     printf("  OS/ABI:                            ");
-    switch (header->osabi) {
+    switch (header->osabi)
+    {
         case 0:
             printf("UNIX - System V\n");
             break;
         case 1:
-            printf("UNIX - HP-UX\n");
+            printf("HP-UX\n");
             break;
         case 2:
-            printf("UNIX - NetBSD\n");
+            printf("NetBSD\n");
             break;
         case 3:
-            printf("UNIX - Linux\n");
+            printf("Linux\n");
             break;
         case 6:
-            printf("UNIX - Solaris\n");
+            printf("Solaris\n");
             break;
         case 7:
-            printf("UNIX - AIX\n");
+            printf("AIX\n");
             break;
         case 8:
-            printf("UNIX - IRIX\n");
+            printf("IRIX\n");
             break;
         case 9:
-            printf("UNIX - FreeBSD\n");
+            printf("FreeBSD\n");
             break;
         case 10:
-            printf("UNIX - Tru64\n");
+            printf("Tru64\n");
             break;
         case 11:
-            printf("UNIX - Novell Modesto\n");
+            printf("Novell Modesto\n");
             break;
         case 12:
-            printf("UNIX - OpenBSD\n");
+            printf("OpenBSD\n");
             break;
         case 13:
-            printf("UNIX - OpenVMS\n");
+            printf("OpenVMS\n");
             break;
         case 14:
-            printf("UNIX - NonStop Kernel\n");
+            printf("NonStop Kernel\n");
             break;
         case 15:
-            printf("UNIX - AROS\n");
+            printf("AROS\n");
             break;
         case 16:
-            printf("UNIX - FenixOS\n");
+            printf("Fenix OS\n");
             break;
         case 17:
-            printf("UNIX - CloudABI\n");
-            break;
-        case 18:
-            printf("UNIX - Sortix\n");
+            printf("CloudABI\n");
             break;
         default:
             printf("<unknown: %d>\n", header->osabi);
             break;
     }
     printf("  ABI Version:                       %d\n", header->abiversion);
-    printf("  Type:                              %c%c%c%c (",
-           header->type[3], header->type[2], header->type[1], header->type[0]);
-    switch (*((unsigned int *)header->type)) {
+    printf("  Type:                              ");
+    switch (header->type)
+    {
         case 0:
-            printf("NONE");
+            printf("NONE (No file type)\n");
             break;
+        case 1:
+            printf("REL (Relocatable file)\n");
+            break;
+        case 2:
+            printf("EXEC (Executable file)\n");
+            break;
+        case 3:
+            printf("DYN (Shared object file)\n");
+            break;
+        case 4:
+            printf("CORE (Core file)\n");
+            break;
+        default:
+            printf("<unknown: %d>\n", header->type);
+            break;
+    }
+    printf("  Entry point address:               0x%lx\n", header->entry);
+}
+
+int main(int argc, char *argv[])
+{
+    int fd;
+    ElfHeader header;
+
+    if (argc != 2)
+        print_error("Usage: elf_header elf_filename");
+
+    fd = open(argv[1], O_RDONLY);
+    if (fd == -1)
+        print_error("Cannot open file");
+
+    if (read(fd, &header, sizeof(ElfHeader)) != sizeof(ElfHeader))
+        print_error("Cannot read file");
+
+    print_elf_header(&header);
+
+    close(fd);
+    return 0;
+}
