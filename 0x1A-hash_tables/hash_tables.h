6330e55bb85efd30e4eca5b4ec77b39530667c11
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-06-09 23:58:52 +0200

    Update hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -89,6 +89,5 @@ char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
-void hash_table_cleanup(hash_table_t *ht);
 
 #endif /* HASH_TABLES_H */
