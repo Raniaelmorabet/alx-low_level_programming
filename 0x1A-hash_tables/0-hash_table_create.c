d6dddfbafb813f1cfa4d63e5058326211e452cf8
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-06-09 23:37:07 +0200

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -44,32 +44,3 @@ hash_table_t *hash_table_create(unsigned long int size)
 
     return hash_table;
 }
-
-int main(void)
-{
-    hash_table_t *ht;
-
-    ht = hash_table_create(1024);
-    printf("%p\n", (void *)ht);
-
-    /* Use the hash table */
-
-    /* Free the hash table */
-    unsigned long int i;
-    for (i = 0; i < ht->size; i++)
-    {
-        hash_node_t *node = ht->array[i];
-        while (node != NULL)
-        {
-            hash_node_t *temp = node;
-            node = node->next;
-            free(temp->key);
-            free(temp->value);
-            free(temp);
-        }
-    }
-    free(ht->array);
-    free(ht);
-
-    return 0;
-}
