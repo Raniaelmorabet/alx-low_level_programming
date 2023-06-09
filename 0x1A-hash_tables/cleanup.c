61f2f415fadd47aa6923fb3d99c8048401253980
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-06-09 23:53:36 +0200

    Update cleanup.c

--- 0x1A-hash_tables/cleanup.c
+++ 0x1A-hash_tables/cleanup.c
@@ -1,42 +1,14 @@
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "hash_tables.h"
 
-/**
- * hash_table_cleanup - Frees the allocated memory for the hash table.
- * @ht: The hash table to free.
- */
 void hash_table_cleanup(hash_table_t *ht)
 {
-    unsigned long int i;
-    for (i = 0; i < ht->size; i++)
-    {
-        hash_node_t *node = ht->array[i];
-        while (node != NULL)
-        {
-            hash_node_t *temp = node;
-            node = node->next;
-            free(temp->key);
-            free(temp->value);
-            free(temp);
-        }
-    }
+    if (ht == NULL)
+        return;
+
+    /* Free the array */
     free(ht->array);
+
+    /* Free the hash table */
     free(ht);
-}
-
-int main(void)
-{
-    hash_table_t *ht;
-
-    ht = hash_table_create(1024);
-    printf("%p\n", (void *)ht);
-
-    /* Use the hash table */
-
-    /* Clean up and free the hash table */
-    hash_table_cleanup(ht);
-
-    return EXIT_SUCCESS;
 }
