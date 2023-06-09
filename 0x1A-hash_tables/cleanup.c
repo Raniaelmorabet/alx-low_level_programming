787e893db8171908d7aa5212d264bc5b263afa39
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-06-09 23:54:56 +0200

    Update cleanup.c

--- 0x1A-hash_tables/cleanup.c
+++ 0x1A-hash_tables/cleanup.c
@@ -3,8 +3,25 @@
 
 void hash_table_cleanup(hash_table_t *ht)
 {
+    unsigned long int i;
+    hash_node_t *node, *temp;
+
     if (ht == NULL)
         return;
+
+    /* Free the hash nodes */
+    for (i = 0; i < ht->size; i++)
+    {
+        node = ht->array[i];
+        while (node != NULL)
+        {
+            temp = node;
+            node = node->next;
+            free(temp->key);
+            free(temp->value);
+            free(temp);
+        }
+    }
 
     /* Free the array */
     free(ht->array);
