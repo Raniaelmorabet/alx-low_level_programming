b554718c67c9dfe10be78bd5e08ec053fcb39e09
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-09-13 01:51:49 +0200

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,18 +1,14 @@
-#ifndef SEARCH_ALGOS_H
-#define SEARCH_ALGOS_H
+#ifndef SEARCH_ALGO
+#define SEARCH_ALGO
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
-/* PROTOTYPES - MANDATORY */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-
-/* PROTOTYPES - ADVANCED */
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 
-#endif /* SEARCH_ALGOS_H */
+#endif
