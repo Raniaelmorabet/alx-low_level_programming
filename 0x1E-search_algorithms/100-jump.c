562d20613e32c0c7980f84ee5887c51ef9cc3fc9
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-09-13 01:50:52 +0200

    Update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,76 +1,41 @@
 #include "search_algos.h"
+#include <math.h>
 
 /**
-  * print_array - print array with limits in l and r
-  * @array: set of numbers
-  * @l: left limit
-  * @r: right limit
-  * Return: nothing
-  */
-void print_array(int *array, size_t l, size_t r)
+ * jump_search - search a value in a sort list
+ * @array: list of numbers
+ * @size: lenght of the list
+ * @value: value to search
+ * Return: the first index of value otherwise -1
+ */
+int jump_search(int *array, size_t size, int value)
 {
-	printf("Searching in array: ");
-	for (l = l; l < r; l++)
-		printf("%d, ", array[l]);
-	printf("%d\n", array[l]);
-}
-
-/**
-  * binary_search_p - search the first ocurrency of a value in the array
-  * @array: set of numbers
-  * @l: left limit
-  * @r: right limit
-  * @value: value to search
-  * Return: return the first index located otherwise -1
-  */
-int binary_search_p(int *array, size_t l, size_t r, int value)
-{
-	size_t mid = 0;
-
-	while (l <= r)
-	{
-		print_array(array, l, r);
-		mid = (l + r) / 2;
-		if (array[mid] < value)
-			l = mid + 1;
-		else if (array[mid] > value)
-			r = mid - 1;
-		else
-			return (mid);
-	}
-	return (-1);
-}
-
-/**
-  * min_value - get the min value of two numbers
-  * @l: number l
-  * @r: number r
-  * Return: return the minimun value
-  */
-size_t min_value(size_t l, size_t r)
-{
-	return ((l < r) ? l : r);
-}
-
-/**
-  * exponential_search - search the first ocurrency of a value in the array
-  * @array: set of numbers
-  * @size: size of the array
-  * @value: value to search
-  * Return: return the first index located otherwise -1
-  */
-int exponential_search(int *array, size_t size, int value)
-{
-	size_t pow = 1;
+	size_t a = 0, step = sqrt(size);
 
 	if (array == NULL)
 		return (-1);
-	while (pow < size && array[pow] < value)
+	while (array[a] < value)
 	{
-		printf("Value checked array[%d] = [%d]\n", (int)pow, array[pow]);
-		pow *= 2;
+		printf("Value checked array[%d] = [%d]\n", (int)a, array[a]);
+		a += step;
+		if (a >= size)
+			break;
 	}
+	a -= step;
 	printf("Value found between indexes [%d] and [%d]\n",
-					(int)(pow / 2), (int)min_value(pow, size - 1));
-	return (binary_search_p(array, pow / 2, min_value(pow, size - 1), value));
+					(int)a, (int)(a + step));
+	while (array[a] < value)
+	{
+		printf("Value checked array[%d] = [%d]\n", (int)a, array[a]);
+		a++;
+		if (a == size)
+			return (-1);
+	}
+
+	if (array[a] == value)
+	{
+		printf("Value checked array[%d] = [%d]\n", (int)a, array[a]);
+		return (a);
+	}
+	return (-1);
 }
