39c697c2318a9cb7e32055fdd22857a75a29908a
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-09-13 01:54:21 +0200

    Update 104-advanced_binary.c

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,39 +1,49 @@
 #include "search_algos.h"
 
 /**
- * advanced_binary - searches for a value in a sorted array of integers using
- * the Binary search algorithm
- * @array: pointer to the first element of the array to search in
- * @size: number of elements in the array
+ * _advanced_binary - performs advanced binary search
+ * @array: the integer array
+ * @size: its size
  * @value: value to search for
  *
- * Return: first index where value is located, or -1 if value is not present
- * in array or if array is NULL
+ * Return: the index found or -1
+ */
+int *_advanced_binary(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (!size || !array)
+		return (NULL);
+	for (printf("Searching in array: "); i < size; i++)
+		printf("%d%s", array[i], i + 1 == size ? "\n" : ", ");
+
+	i = (size - 1) / 2;
+	if (array[i] == value)
+	{
+		if (i)
+			return (_advanced_binary(array, i + 1, value));
+		return (array + i);
+	}
+	else if (array[i] > value)
+		return (_advanced_binary(array, i + 1, value));
+	else
+		return (_advanced_binary(array + i + 1, size - i - 1, value));
+}
+
+/**
+ * advanced_binary - performs advanced binary search
+ * @array: the integer array
+ * @size: its size
+ * @value: value to search for
+ *
+ * Return: the index found or -1
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t i = 0, j = size - 1, m, k;
+	int *a = _advanced_binary(array, size, value);
 
-	if (array)
-	{
-		while (i <= j)
-		{
-			printf("Searching in array: ");
-			for (k = i; k < j; k++)
-				printf("%d, ", array[k]);
-			printf("%d\n", array[k]);
-			m = (i + j) / 2;
-			if (array[m] == value)
-			{
-				if (m == 0 || array[m - 1] < value)
-					return (m);
-				j = m;
-			}
-			else if (array[m] < value)
-				i = m + 1;
-			else
-				j = m;
-		}
-	}
-	return (-1);
+	if (!a)
+		return (-1);
+	else
+		return (a - array);
 }
